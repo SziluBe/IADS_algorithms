24a9d8c5df6d7cb59014d3c6357b1f593d37af36
SziluBe <s2099359@ed.ac.uk>
2022-05-07 17:53:24 +0100

    Updates from origin: docs, better tracing

--- parsing_algos/cyk.py
+++ parsing_algos/cyk.py
@@ -1,79 +1,95 @@
-from typing import List
+from ast import arg, parse
+from lib2to3.pgen2 import grammar
+from typing import Dict, List
 from enum import Enum, auto
 import pprint
+import pandas as pd
+
 
 class NT:
     def __init__(self, l: Enum, r: Enum):
         self.l = l
         self.r = r
 
+
 class Rule:
-    def __init__(self, LHS: Enum, RHS, terminal: bool = False): # RHS: NT or str
+    def __init__(self, LHS: Enum, RHS, terminal: bool = False):  # RHS: NT or str
         self.LHS = LHS
         self.RHS = RHS
-        self.terminal = terminal
+        self.terminal = terminal  # flag to mark node as terminal
 
-def cyk(s: List, G: "list[Rule]") -> "list[list[list[Enum]]]":  # G should really be a set of Rules but I cba to fix it rn
+
+def cyk(
+    s: List, G: "list[Rule]"
+) -> "list[list[list[Enum]]]":  # G should really be a set of Rules but I cba to fix it rn
     n = len(s)
     table = [[[] for _ in range(n - 1)] for _ in range(n - 1)]
-    
-    for j in range(1, n + 1):   # columns
+
+    for j in range(1, n + 1):  # columns
         for rule in G:
             if rule.terminal:
                 if s[j - 1] == rule.RHS:
                     table[j - 1][j].append(rule.LHS)  # diagonal cell
-        
+
         for i in range(j - 2, -1, -1):  # rows
-            for k in range(i + 1, j):   # possible splits
+            for k in range(i + 1, j):  # possible splits
                 for rule in G:
                     if not rule.terminal:
                         if rule.RHS.l in table[i][k] and rule.RHS.r in table[k][j]:
                             table[i][j].append(rule.LHS)
-    
+
     for i in range(len(table)):
         del table[i][0]
     del table[-1]
-    
+
     return table
 
-def cyk_parser(s: List, G: "list[Rule]") -> "list[list[list[tuple(Enum, tuple(int, int), tuple(int, int))]]]":  # G should really be a set of Rules but I cba to fix it rn
+
+def cyk_parser(
+    s: List, G: "list[Rule]"
+) -> "list[list[list[tuple[Enum, tuple[int, int], tuple[int, int]]]]]":  # G should really be a set of Rules but I cba to fix it rn
     n = len(s)
     table = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
-    
-    for j in range(1, n + 1):   # columns
+
+    for j in range(1, n + 1):  # columns
         for rule in G:
             if rule.terminal:
                 if s[j - 1] == rule.RHS:
-                    table[j - 1][j].append((rule.LHS, (j - 1, j), (j - 1, j)))  # diagonal cell
-        
+                    table[j - 1][j].append(
+                        (rule.LHS, (j - 1, j), (j - 1, j))
+                    )  # diagonal cell
+
         for i in range(j - 2, -1, -1):  # rows
-            for k in range(i + 1, j):   # possible splits
+            for k in range(i + 1, j):  # possible splits
                 for rule in G:
                     if not rule.terminal:
-                        if any([rule.RHS.l == x[0] for x in table[i][k]]) and any([rule.RHS.r == x[0] for x in table[k][j]]):
+                        if any([rule.RHS.l == x[0] for x in table[i][k]]) and any(
+                            [rule.RHS.r == x[0] for x in table[k][j]]
+                        ):
                             table[i][j].append((rule.LHS, (i, k), (k, j)))
-    
+
     for i in range(len(table)):
         del table[i][0]
     del table[-1]
-    
+
     return table
+
 
 def cyk_traced(s: List, G: "list[Rule]") -> "list[list[list[Enum]]]":
     print("Start:", s)
     n = len(s)
     table = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
-    
-    for j in range(1, n + 1):   # columns
+
+    for j in range(1, n + 1):  # columns
         for rule in G:
             if rule.terminal:
                 if s[j - 1] == rule.RHS:
                     print(n, j)
                     table[j - 1][j].append(rule.LHS)  # diagonal cell
                     print("diag", rule.LHS, j - 1, j)
-        
+
         for i in range(j - 2, -1, -1):  # rows
-            for k in range(i + 1, j):   # possible splits
+            for k in range(i + 1, j):  # possible splits
                 for rule in G:
                     if not rule.terminal:
                         if rule.RHS.l in table[i][k] and rule.RHS.r in table[k][j]:
@@ -81,68 +97,107 @@ def cyk_traced(s: List, G: "list[Rule]") -> "list[list[list[Enum]]]":
                             print("non-diag", rule.LHS, i, j)
                             print("non-diag l", rule.RHS.l, i, k)
                             print("non-diag r", rule.RHS.r, k, j)
-    
+
     for i in range(len(table)):
         del table[i][0]
     del table[-1]
 
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(table)
+    table = pd.DataFrame(table, s, s)
+    print(table)
     return table
 
-def cyk_parser_traced(s: List, G: "list[Rule]") -> "list[list[list[tuple(Enum, tuple(int, int), tuple(int, int))]]]":
+
+def cyk_parser_traced(s: List, G: "list[Rule]"):
     print("Start:", s)
     n = len(s)
     table = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
-    
-    for j in range(1, n + 1):   # columns
+
+    for j in range(1, n + 1):  # columns
         for rule in G:
             if rule.terminal:
                 if s[j - 1] == rule.RHS:
                     print(n, j)
-                    table[j - 1][j].append((rule.LHS, (j - 1, j), (j - 1, j)))  # diagonal cell
+                    table[j - 1][j].append(
+                        (rule.LHS, (j - 1, j), (j - 1, j))
+                    )  # diagonal cell
                     print("diag", rule.LHS, j - 1, j)
-        
+
         for i in range(j - 2, -1, -1):  # rows
-            for k in range(i + 1, j):   # possible splits
+            for k in range(i + 1, j):  # possible splits
                 for rule in G:
                     if not rule.terminal:
-                        if any([rule.RHS.l == x[0] for x in table[i][k]]) and any([rule.RHS.r == x[0] for x in table[k][j]]):
+                        if any([rule.RHS.l == x[0] for x in table[i][k]]) and any(
+                            [rule.RHS.r == x[0] for x in table[k][j]]
+                        ):
                             table[i][j].append((rule.LHS, (i, k), (k, j)))
                             print("non-diag", rule.LHS, i, j)
                             print("non-diag l", rule.RHS.l, i, k)
                             print("non-diag r", rule.RHS.r, k, j)
-    
+
     for i in range(len(table)):
         del table[i][0]
     del table[-1]
 
-    pp = pprint.PrettyPrinter(indent=2)
-    pp.pprint(table)
+    table = pd.DataFrame(table, s, s)
+    print(table)
     return table
 
+
+def parseFromFile(file: str, parse: str):
+    grammarRules = extractGrammar(file)
+
+    return cyk_parser_traced(parse.split(), grammarRules)
+
+def cykFromFile(file: str, parse:str):
+    grammarRules = extractGrammar(file)
+    
+    return cyk_traced(parse.split(), grammarRules)
+
+
+def extractGrammar(file):
+    content = []
+    with open(file, "r") as f:
+        content = f.readlines()
+
+    ntsStrings = dict()
+    rules = []
+    for line in content:
+        split = line.split("->")
+        ntsStrings[split[0].strip()] = split[0].strip()
+        rules.append([split[0].strip()] + split[1].split())
+
+    NTs = Enum("NTs", ntsStrings)
+
+    grammarRules = []
+    for rule in rules:
+        # case that rule is a terminal
+        if len(rule) == 2:
+            grammarRules.append(Rule(NTs(rule[0]), rule[1], True))
+        else:
+            grammarRules.append(Rule(NTs(rule[0]), NT(NTs(rule[1]), NTs(rule[2]))))
+    return grammarRules
+
+
+
+
+
 if __name__ == "__main__":
-    class NTs(Enum):
-        NP = auto()
-        Nom = auto()
-        AP = auto()
-        A = auto()
-        Det = auto()
-        Adv = auto()
+    from sys import argv
 
-    grammarRules = [
-        Rule(NTs.NP, NT(NTs.Det, NTs.Nom)),
-        Rule(NTs.Nom, "book", True),
-        Rule(NTs.Nom, "orange", True),
-        Rule(NTs.Nom, NT(NTs.AP, NTs.Nom)),
-        Rule(NTs.AP, "heavy", True),
-        Rule(NTs.AP, "orange", True),
-        Rule(NTs.AP, NT(NTs.Adv, NTs.A)),
-        Rule(NTs.A, "heavy", True),
-        Rule(NTs.A, "orange", True),
-        Rule(NTs.Det, "my", True),
-        Rule(NTs.Adv, "very", True)
-    ]
-
-    cyk_parser_traced(["my", "very", "heavy", "orange", "book"], grammarRules)
-    cyk_traced(["my", "very", "heavy", "orange", "book"], grammarRules)
+    # out = cyk_parser(["my", "very", "heavy", "orange", "book"], grammarRules)
+    # print(pprint.pformat(out))
+    # cyk_traced(["my", "very", "heavy", "orange", "book"], grammarRules)
+    if len(argv) == 1:
+        print("Call function using python3 cyk.py inputfile.txt \"String to parse\" outfile.md")
+        print("For example")
+        print("python3 cyk.py input.txt \"I ate the salad with a fork\" out1.md")
+        print("An example input file is provided. The numbers next the terminal/ non terminals indicate where they were taken from")
+        print("Remember that the left side is 0 indexed, the top is 1 indexed.")
+    else:
+        out = parseFromFile(argv[1], argv[2])
+        if len(argv) == 3:
+            
+            out.to_markdown(argv[3])
+        else:
+            out.to_markdown("out.md")
+    
--- parsing_algos/ll1.py
+++ parsing_algos/ll1.py
@@ -3,18 +3,25 @@ from queue import LifoQueue
 from enum import Enum, auto
 import pprint
 
+
 class LngElem:
     def __init__(self, val, terminal: bool = False):
         self.val = val
         self.terminal = terminal
 
+
 class Rule:
-    def __init__(self, LHS: LngElem, RHS: "list[LngElem]", terminal: bool = False):   # different from the CYK version!
+    def __init__(
+        self, LHS: LngElem, RHS: "list[LngElem]", terminal: bool = False
+    ):  # different from the CYK version!
         self.LHS = LHS
         self.RHS = RHS
         self.terminal = terminal
 
-def ll1_parse(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols: dict) -> bool:
+
+def ll1_parse(
+    table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols: dict
+) -> bool:
     terminals = parse_cols
     if S in terminals:
         return Exception("Error: start symbol is terminal")
@@ -26,11 +33,11 @@ def ll1_parse(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols:
         if x not in terminals:  # Lookup case
             if table[x][parse_cols[in_str[pos]]] == None:
                 return Exception("Error 1: looked up blank cell")
-            else:   # rule x -> beta
+            else:  # rule x -> beta
                 stack.get()
                 for symbol in reversed(table[x][parse_cols[in_str[pos]]].RHS):
                     stack.put(symbol)
-        else:   # Match case (x is terminal)
+        else:  # Match case (x is terminal)
             if x == in_str[pos]:
                 stack.get()
                 pos += 1
@@ -43,7 +50,10 @@ def ll1_parse(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols:
     else:
         return Exception("Error 3: stack empty before string empty")
 
-def ll1_parse_traced(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols: dict) -> bool:
+
+def ll1_parse_traced(
+    table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols: dict
+) -> bool:
     terminals = parse_cols
     if S in terminals:
         return Exception("Error: start symbol is terminal")
@@ -52,17 +62,52 @@ def ll1_parse_traced(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, pars
     stack.put(S)
     while not stack.empty():
         x = stack.queue[-1]
-        print("x", x)
         if x not in terminals:  # Lookup case
-            print("RHS", table[x][parse_cols[in_str[pos]]].RHS)
             if table[x][parse_cols[in_str[pos]]] == None:
                 return Exception("Error 1: looked up blank cell")
-            else:   # rule x -> beta
+            else:  # rule x -> beta
                 got = stack.get()
-                print("got", got)
+                print(
+                    f"Lookup: {in_str[pos]}, {got} => {table[x][parse_cols[in_str[pos]]].RHS}"
+                )
                 for symbol in reversed(table[x][parse_cols[in_str[pos]]].RHS):
                     stack.put(symbol)
-        else:   # Match case (x is terminal)
+        else:  # Match case (x is terminal)
+            if x == in_str[pos]:
+                print(f"matched: {stack.get()}")  # removes item from stack
+                pos += 1
+            else:
+                if not x in terminals:
+                    return Exception("Error 2: terminal expected", x, "found")
+                return Exception("Error 2: expected", x, "found", in_str[pos])
+        print(f"input: {in_str[pos:]}")
+        print("stack", stack.queue[::-1])
+        print("-" * 10)
+    if in_str[pos] == "$":
+        return True
+    else:
+        return Exception("Error 3: stack empty before string empty")
+
+
+def ll1_parse_lightly_traced(
+    table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols: dict
+) -> bool:
+    terminals = parse_cols
+    if S in terminals:
+        return Exception("Error: start symbol is terminal")
+    pos = 0
+    stack = LifoQueue()
+    stack.put(S)
+    while not stack.empty():
+        x = stack.queue[-1]
+        if x not in terminals:  # Lookup case (X is a non terminal)
+            if table[x][parse_cols[in_str[pos]]] == None:
+                return Exception("Error 1: looked up blank cell")
+            else:  # rule x -> beta
+                got = stack.get()
+                for symbol in reversed(table[x][parse_cols[in_str[pos]]].RHS):
+                    stack.put(symbol)
+        else:  # Match case (x is terminal)
             if x == in_str[pos]:
                 stack.get()
                 pos += 1
@@ -76,58 +121,28 @@ def ll1_parse_traced(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, pars
     else:
         return Exception("Error 3: stack empty before string empty")
 
-def ll1_parse_lightly_traced(table: "dict[Enum, list[Rule]]", S: Enum, in_str: str, parse_cols: dict) -> bool:
-    terminals = parse_cols
-    if S in terminals:
-        return Exception("Error: start symbol is terminal")
-    pos = 0
-    stack = LifoQueue()
-    stack.put(S)
-    while not stack.empty():
-        x = stack.queue[-1]
-        if x not in terminals:  # Lookup case
-            if table[x][parse_cols[in_str[pos]]] == None:
-                return Exception("Error 1: looked up blank cell")
-            else:   # rule x -> beta
-                got = stack.get()
-                for symbol in reversed(table[x][parse_cols[in_str[pos]]].RHS):
-                    stack.put(symbol)
-        else:   # Match case (x is terminal)
-            if x == in_str[pos]:
-                stack.get()
-                pos += 1
-            else:
-                if not x in terminals:
-                    return Exception("Error 2: terminal expected", x, "found")
-                return Exception("Error 2: expected", x, "found", in_str[pos])
-        print("stack", stack.queue)
-    if in_str[pos] == "$":
-        return True
-    else:
-        return Exception("Error 3: stack empty before string empty")
 
 if __name__ == "__main__":
+
     class NTs(Enum):
         S = auto()
         T = auto()
 
     parse_cols = {"(": 0, ")": 1, "$": 2}
 
-    parseTable = {
-        NTs.S: [
-            Rule(NTs.S, [NTs.T, NTs.S]),
-            Rule(NTs.S, []),
-            Rule(NTs.S, [])
-        ],
+    """
+    e = epsilon
+    __|____(_____|____)___|___$____|
+    S | S -> TS  | S -> e | S -> e |
+    T | T -> (S) |        |        |
+    """
 
-        NTs.T: [
-            Rule(NTs.T, ["(", NTs.S, ")"]),
-            None,
-            None
-        ]
+    parseTable = {
+        NTs.S: [Rule(NTs.S, [NTs.T, NTs.S]), Rule(NTs.S, []), Rule(NTs.S, [])],
+        NTs.T: [Rule(NTs.T, ["(", NTs.S, ")"]), None, None],
     }
 
-    print(ll1_parse(parseTable, NTs.S, "(())$", parse_cols))
-    print(ll1_parse_traced(parseTable, NTs.S, ")$", parse_cols))
-    print(ll1_parse_traced(parseTable, NTs.S, "($", parse_cols))
-    print(ll1_parse_lightly_traced(parseTable, NTs.S, "(())$", parse_cols))
+    print(ll1_parse_traced(parseTable, NTs.S, "(())$", parse_cols))
+    # print(ll1_parse_traced(parseTable, NTs.S, ")$", parse_cols))
+    # print(ll1_parse_traced(parseTable, NTs.S, "($", parse_cols))
+    # print(ll1_parse_lightly_traced(parseTable, NTs.S, "(())$", parse_cols))
